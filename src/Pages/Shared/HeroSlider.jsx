896d041a710a6101dc1caa1afb9630daafed43ce
aaaziim <azim210215@gmail.com>
2025-01-23 19:24:57 +0600

    Hero Slider Gradient Overlay Added

--- src/Pages/Shared/HeroSlider.jsx
+++ src/Pages/Shared/HeroSlider.jsx
@@ -11,7 +11,19 @@ const HeroSlider = () => {
           <img
             src={img1}
             class="w-full" />
-          <div className="absolute left-5 right-5 bottom-1 flex -translate-y-1/2 transform justify-end gap-5">
+            
+            <div className="absolute h-full top-0 left-0 flex items-center px-4  
+             gap-5 bg-linear-to-r from-[#151515] to-[rgba(21,21,21,0)]">
+              <div className='text-white text-left'>
+      <h1 className="text-5xl font-bold">Affordable Price</h1>
+      <p className="py-6">
+        Best Place For Your Car Servicing
+      </p>
+      <button className="btn btn-danger btn-outline">Discover Nore</button>
+    </div>
+          </div>
+          
+          <div className="absolute flex left-5 right-5 bottom-1 flex -translate-y-1/2 transform justify-end gap-5">
             <a href="#slide4" className="btn btn-circle">❮</a>
             <a href="#slide2" className="btn btn-circle">❯</a>
           </div>
@@ -20,6 +32,16 @@ const HeroSlider = () => {
           <img
            src={img2}
             class="w-full" />
+              <div className="absolute h-full top-0 left-0 flex items-center px-4  
+             gap-5 bg-linear-to-r from-[#151515] to-[rgba(21,21,21,0)]">
+              <div className='text-white text-left'>
+      <h1 className="text-5xl font-bold">Affordable Price</h1>
+      <p className="py-6">
+        Best Place For Your Car Servicing
+      </p>
+      <button className="btn btn-danger btn-outline">Discover Nore</button>
+    </div>
+          </div>
           <div className="absolute left-5 right-5 bottom-1 flex -translate-y-1/2 transform justify-end gap-5">
             <a href="#slide1" className="btn btn-circle ">❮</a>
             <a href="#slide3" className="btn btn-circle">❯</a>
@@ -29,6 +51,16 @@ const HeroSlider = () => {
           <img
             src={img3}
             class="w-full" />
+              <div className="absolute h-full top-0 left-0 flex items-center px-4  
+             gap-5 bg-linear-to-r from-[#151515] to-[rgba(21,21,21,0)]">
+              <div className='text-white text-left'>
+      <h1 className="text-5xl font-bold">Affordable Price</h1>
+      <p className="py-6">
+        Best Place For Your Car Servicing
+      </p>
+      <button className="btn btn-danger btn-outline">Discover Nore</button>
+    </div>
+          </div>
           <div className="absolute left-5 right-5 bottom-1 flex -translate-y-1/2 transform justify-end gap-5">
             <a href="#slide2" className="btn btn-circle">❮</a>
             <a href="#slide4" className="btn btn-circle">❯</a>
@@ -37,6 +69,16 @@ const HeroSlider = () => {
         <div id="slide4" className="carousel-item relative w-full">
           <img
             src={img4} />
+              <div className="absolute h-full top-0 left-0 flex items-center px-4  
+             gap-5 bg-linear-to-r from-[#151515] to-[rgba(21,21,21,0)]">
+              <div className='text-white text-left'>
+      <h1 className="text-5xl font-bold">Affordable Price</h1>
+      <p className="py-6">
+        Best Place For Your Car Servicing
+      </p>
+      <button className="btn btn-danger btn-outline">Discover Nore</button>
+    </div>
+          </div>
           <div className="absolute left-5 right-5 bottom-1 flex -translate-y-1/2 transform justify-end gap-5">
             <a href="#slide3" className="btn btn-circle">❮</a>
             <a href="#slide1" className="btn btn-circle">❯</a>
